70d03928f8f6e651fdf31c8735ee6f7ed4791909
RaresButuc <eusuntbutuc01@gmail.com>
2025-01-29 11:56:38 +0200

    First Background Home Page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,7 +8,34 @@ export default function Home() {
       <Navbar />
       <div className={styles.page}>
         <main className={styles.main}>
-        <video className="video-bg" src="/video/bg1.mp4" autoPlay loop muted  style={{width:"100%"}}/>
+          <video
+            className="video-bg"
+            src="/video/bg1.mp4"
+            autoPlay
+            loop
+            muted
+            style={{ width: "100%" }}
+          />
+          <h1 className="fw-bold" style={{ color: "white" }}>
+            Your Journey, <mark>Shared</mark>. Your World, <mark>Expanded</mark>
+            .
+          </h1>
+          <div className="d-flex justify-content-center">
+            <h5 className="fw-bold mb-3 lh-base" style={{ color: "white" }}>
+              A platform where <mark>travel</mark> and <mark>social media</mark>{" "}
+              meet to create authentic experiences.
+            </h5>
+          </div>
+
+          <div className="container-xl d-flex justify-content-center">
+            <a
+              className="btn btn-outline-warning fw-bold p-3"
+              href="/post-ads"
+              style={{ color: "white" }}
+            >
+              Start Your Jorney HERE
+            </a>
+          </div>
         </main>
       </div>
     </>
